d950158d3b2974f022ae05a30564a5d8db897000
acdc4567 <acdc4567@gmail.com>
2022-03-30 07:38:32 +0530

    Initial Commit

---
+++ Shared/sandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandsApp.swift
+//  Shared
+//
+//  Created by Jatin Sanmukhani on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct sandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
